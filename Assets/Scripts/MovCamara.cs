5dda038b1cd86fe6cb963a2dfba6faa09332bd0d
Sergi Cugat <sergi.cugat@estudiants.urv.cat>
2017-12-28 03:11:09 +0100

    Mes canvis

--- Assets/Scripts/MovCamara.cs
+++ Assets/Scripts/MovCamara.cs
@@ -15,7 +15,7 @@ public class MovCamara : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         if (Input.GetKey(KeyCode.W) && transform.position.z < limitEndavant) transform.Translate(Vector3.forward * Time.deltaTime * velocitatCamara);
-        else if (Input.GetKey(KeyCode.S) && transform.position.z < limitEndarrera) transform.Translate(Vector3.back * Time.deltaTime * velocitatCamara);
+        else if (Input.GetKey(KeyCode.S) && transform.position.z > limitEndarrera) transform.Translate(Vector3.back * Time.deltaTime * velocitatCamara);
         if (Input.GetKey(KeyCode.A) && transform.position.x > limitEsquerra) transform.Translate(Vector3.left * Time.deltaTime * velocitatCamara);
         else if (Input.GetKey(KeyCode.D) && transform.position.x < limitDreta) transform.Translate(Vector3.right * Time.deltaTime * velocitatCamara);
         if (Input.GetKey(KeyCode.Q) && transform.position.y > limitBaix) transform.Translate(Vector3.down * Time.deltaTime * velocitatCamara);
