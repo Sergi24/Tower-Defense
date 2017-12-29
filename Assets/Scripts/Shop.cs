454b3caea3c98e3c095c04f339f7f834ba0bfbf9
Daniel Zarco <dazarco96@gmail.com>
2017-12-29 02:46:24 +0100

    Se mantiene el último botón seleccionado

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -1,26 +1,42 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Shop : MonoBehaviour {
 
     BuildManager buildManager;
+    public Button purchaseArrowButton;
+    public Button purchaseBombButton;
+    public Button purchaseKnightButton;
+    public Button purchaseCatapultButton;
+
+    private ColorBlock normalColorBlock;
+    private ColorBlock pressedColorBlock;
+    private Button[] buttonList;
 
     void Start() {
         buildManager = BuildManager.instance;
+        buttonList = new Button[] {purchaseArrowButton, purchaseBombButton, purchaseKnightButton, purchaseCatapultButton };
+
+        normalColorBlock = purchaseArrowButton.colors;
+
+        pressedColorBlock = normalColorBlock;
+        pressedColorBlock.normalColor = Color.white;
+        pressedColorBlock.pressedColor = Color.gray;
     }
 
     public void PurchaseArrowTower () {
         buildManager.SetDefenseToBuild(buildManager.arrowTowerPrefab);
-        Debug.Log("Arrow tower purchased");
+        SelectButton("BuildArcherTower");
     }
 
     public void PurchaseBombTower() {
         buildManager.SetDefenseToBuild(buildManager.bombTowerPrefab);
-        Debug.Log("Bomb tower purchased");
+        SelectButton("BuildBombTower");
     }
 
     public void PurchaseCatapult() {
         buildManager.SetDefenseToBuild(buildManager.catapultPrefab);
-        Debug.Log("Catapult purchased");
+        SelectButton("BuildCatapult");
     }
 
     public void PurchaseKnight() {
@@ -28,7 +44,16 @@ public class Shop : MonoBehaviour {
         for (int i = 0; i < list.Length; i++) {
             list[i].GetComponent<CaballeroInstantiator>().crearCaballero();
         }
-        Debug.Log("Knight purchased");
+        SelectButton("clean pressed buttons");
+    }
+
+    void SelectButton(string buttonName) {
+        foreach (Button b in buttonList ) {
+            if (b.name == buttonName) {
+                b.colors = pressedColorBlock;
+            }
+            else b.colors = normalColorBlock;
+        }
     }
 
 
